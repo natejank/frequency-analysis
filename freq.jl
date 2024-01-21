9d27f73fc20452357f6b60c89163fdc238aa1ea6
natejank <njj3397@rit.edu>
2024-01-21 11:04:35 -0500

    improve word trimming

--- freq.jl
+++ freq.jl
@@ -1,6 +1,3 @@
-t = Dict{String, Int64}()
-fp = open("examples/interjection.txt")
-
 function is_alphanumeric(c::Char)
     return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || ('0' <= c && c <= '9')
 end
@@ -15,18 +12,29 @@ function trimWord(word::String)
     return word
 end
 
-while !eof(fp)
-    word = readuntil(fp, ' ')
-    if word == ""
-        continue
+function to_csv(map::Dict)
+    for k in keys(map)
+        println("$(k), $(map[k])")
     end
-
-    word = lowercase(word)
-    word = trimWord(word)
-
-    t[word] = get(t, word, 0) + 1
 end
+
+
+t = Dict{String, Int64}()
+fp = open("examples/interjection.txt")
+
+word = ""
+for chr in readeach(fp, Char)
+    global word
+    if !isspace(chr)
+        word = word * chr
+    elseif word != ""
+        word = lowercase(word)
+        word = trimWord(word)
+
+        t[word] = get(t, word, 0) + 1
+        word = ""
+    end
+end
+
 close(fp)
-
-println(t)
-
+to_csv(t)
