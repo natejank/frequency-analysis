6c9a006751139b0b3c7f0a8154a861ea7ef70280
natejank <njj3397@rit.edu>
2024-01-21 11:25:35 -0500

    fix trimWord

--- freq.jl
+++ freq.jl
@@ -4,10 +4,33 @@ end
 
 function trimWord(word::String)
     if !is_alphanumeric(word[1])
-        word = SubString(word, 2, length(word))
+        dec = 1
+        while !is_alphanumeric(word[1 + dec])
+            dec += 1
+            if dec == length(word)
+                break
+            end
+        end
+
+        word = SubString(word, 1 + dec, length(word))
     end
+
+    if length(word) == 0
+        return ""
+    end
+
     if !is_alphanumeric(word[length(word)])
-        word = SubString(word, 1, length(word) - 1)
+        dec = 1
+        while !is_alphanumeric(word[length(word) - dec])
+
+            dec += 1
+            if dec == length(word)
+                break
+            end
+
+        end
+
+        word = SubString(word, 1, length(word) - dec)
     end
     return word
 end
@@ -19,8 +42,12 @@ function to_csv(map::Dict)
 end
 
 
-t = Dict{String, Int64}()
-fp = open("examples/interjection.txt")
+if (length(ARGS) < 1)
+    error("usage: freq.jl [filename]")
+    exit(1)
+end
+words = Dict{String, Int64}()
+fp = open(ARGS[1])
 
 word = ""
 for chr in readeach(fp, Char)
@@ -29,12 +56,14 @@ for chr in readeach(fp, Char)
         word = word * chr
     elseif word != ""
         word = lowercase(word)
-        word = trimWord(word)
 
-        t[word] = get(t, word, 0) + 1
+        newWord = trimWord(word)
+        word = newWord
+
+        words[word] = get(words, word, 0) + 1
         word = ""
     end
 end
 
 close(fp)
-to_csv(t)
+to_csv(words)
